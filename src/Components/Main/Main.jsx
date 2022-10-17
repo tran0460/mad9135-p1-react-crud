bf8bc5304bfea47f0383a27653eb12f4dd93a21a
tran0460 <tran0460@algonquinlive.com>
2022-10-17 17:20:19 -0400

    create logic for getting items from local storage

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import NewItemView from "../NewItemView/NewItemView";
+import { useEffect, useState } from "react";
 
 const Main = () => {
+  const [itemList, setItemList] = useState([]);
+
+  const getItemsFromStorage = () => {
+    setItemList(
+      Object.keys(localStorage).map(function (key) {
+        return JSON.parse(localStorage.getItem(key));
+      })
+    );
+  };
+  useEffect(() => {
+    window.onstorage = () => {
+      // When local storage changes, update items
+      getItemsFromStorage();
+    };
+  }, []);
   return (
     <div>
       {" "}
