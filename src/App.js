5eeb55211c2f238bd8987183b34e6be17526bb57
tran0460 <tran0460@algonquinlive.com>
2022-10-17 19:10:16 -0400

    create routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "./Components/Main/Main";
+import ListView from "./Components/ListView/ListView";
+import Form from "./Components/Form/Form";
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route path="list" element={<ListView />} />
+          <Route path="new" element={<Form />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
