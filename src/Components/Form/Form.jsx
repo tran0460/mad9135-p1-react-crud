d0ee179d4a20c1f29127fe793d288807add49c96
tran0460 <tran0460@algonquinlive.com>
2022-10-17 17:20:05 -0400

    create logic for creating items in local storage

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => {
+const Form = (props) => {
+  const { nameValue = "", genreValue = "", dateValue = "" } = props;
+  const [nameInput, setNameInput] = useState(nameValue);
+  const [genreInput, setGenreInput] = useState(genreValue);
+  const [dateInput, setDateInput] = useState(dateValue);
+
+  const uploadItem = () => {
+    const obj = {
+      name: nameInput,
+      genre: genreInput,
+      date: dateInput,
+      id: Date.now(),
+    };
+    localStorage.setItem(`${Date.now()}`, JSON.stringify(obj));
+    setNameInput("");
+    setGenreInput("");
+    setDateInput("");
+    window.dispatchEvent(new Event("storage"));
+  };
   return (
     <div>
-      <label htmlFor="name"></label>
-      <input type="text" id="name" placeholder="Game name" />
-      <label htmlFor="genre"></label>
-      <input type="text" id="genre" placeholder="Game genre" />
-      <label htmlFor="release-date"></label>
-      <input type="text" id="release-date" placeholder="Game release date" />
-      <button>Cancel</button>
-      <button>Button</button>
+      <label htmlFor="name">Game name</label>
+      <input
+        value={nameInput}
+        onChange={(ev) => setNameInput(ev.target.value)}
+        type="text"
+        id="name"
+        placeholder="Name"
+      />
+      <label htmlFor="genre">Game Genre</label>
+      <input
+        value={genreInput}
+        onChange={(ev) => setGenreInput(ev.target.value)}
+        type="text"
+        id="genre"
+        placeholder="Genre"
+      />
+      <label htmlFor="release-date">Game release date</label>
+      <input
+        value={dateInput}
+        onChange={(ev) => setDateInput(ev.target.target)}
+        type="text"
+        id="release-date"
+        placeholder="Release date"
+      />
+      <button onClick={() => {}}>Cancel</button>
+      <button
+        onClick={() => {
+          uploadItem();
+        }}>
+        Submit
+      </button>
     </div>
   );
 };
