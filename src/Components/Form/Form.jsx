00de719186c9cd87cd9f91724900b0ffab33c6a2
tran0460 <tran0460@algonquinlive.com>
2022-10-17 16:21:27 -0400

    create form component

---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <label htmlFor=""></label>
+      <input type="text" id="" />
+      <label htmlFor=""></label>
+      <input type="text" />
+      <label htmlFor=""></label>
+      <input type="text" />
+      <button>Cancel</button>
+      <button>Button</button>
+    </div>
+  );
+};
+
+export default Form;
