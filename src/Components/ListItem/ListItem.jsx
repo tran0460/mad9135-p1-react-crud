224dc0b8bb39c89052ec03df93613463b676ab3b
tran0460 <tran0460@algonquinlive.com>
2022-10-17 17:45:48 -0400

    create logic for edit mode

--- src/Components/ListItem/ListItem.jsx
+++ src/Components/ListItem/ListItem.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "../Form/Form";
 
 const ListItem = (props) => {
   const { item } = props;
-
+  const [editMode, setEditMode] = useState(false);
   return (
     <div>
       <p>{item.name}</p>
       <p>{item.genre}</p>
       <p>{item.date}</p>
+      {editMode && <Form onCancel={() => setEditMode(false)} />}
+      <button
+        onClick={() => {
+          setEditMode(true);
+        }}>
+        Edit
+      </button>
+      <button>Delete</button>
     </div>
   );
 };
